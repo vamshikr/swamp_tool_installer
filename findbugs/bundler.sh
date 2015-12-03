1ffec1aa7a8cbe19c19266138dfad0cf0661a715
Vamshi Basupalli <vamshi_kr@hotmail.com>
2015-12-03 10:44:40 -0600

    Findbugs bundler complete

--- findbugs/bundler.sh
+++ findbugs/bundler.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+
+function md5 {
+    (
+	cd $1
+	find . -type f ! -name md5sum -exec md5sum '{}' ';' > ./md5sum;
+    )
+}
+
+function get_basedir {
+    local archive="$1"
+
+    if [[ "$archive" == *.zip ]]; then
+	zipinfo -1 "$archive" | sed -n -r 's@([^/]+)/.+@\1@p' | uniq
+    elif [[ "$archive" == *.tar.gz ]]; then
+	tar tf "$archive" | sed -n -r 's@([^/]+)/.+@\1@p' | uniq
+    fi
+}
+
 function create_invoke_file {
 
     local filepath=$1
@@ -10,7 +28,7 @@ java
     -classpath <executable>
     -Dfindbugs.home="\${TOOL_DIR}/<tool-dir>"
     <main-class>
-    -pluginList <plugin%:>
+    -pluginList <plugin>
     -effort:<effort> 
     -exclude <exfile>
     -include <infile>
@@ -36,41 +54,68 @@ EOF
     if [[ -n $plugins ]]; then
 	echo "plugin=$plugins" >> $filepath
     fi
-
 }
 
-function md5 {
+function copy_file {
+    local url="$1"
+    local dir="$2"
+
+    if egrep --quiet --invert-match '^(http[s]?|file|ftp)://' <(echo "$url"); then
+	url="file://$(readlink -e $url)"
+    fi
+
     (
-	cd $1
-	find . -type f ! -name md5sum -exec md5sum '{}' ';' > ./md5sum;
+	cd "$dir"
+	curl --silent --location --remote-name "$url"
     )
 }
 
-function get_basedir {
-    local archive=$1
-
-    if [[ $archive == *.zip ]]; then
-	zipinfo -1 $archive | sed -n -r 's@([ ^/]+)/.+@\1@p' | uniq
-    elif [[ $archive == *.tar.gz ]]; then
-	tar tf $archive | sed -n -r 's@([ ^/]+)/.+@\1@p' | uniq
-    fi
-}
-
-function main {
+function create_all {
     
-    local out_dir="test"
+    local tool_version="$1"; shift
+    local out_dir="$1"; shift
+    local tool_url="$1"; shift
+    
+    declare -a plugins
+    plugins=($@)
+
     local tool_type=findbugs
-    local tool_version="3.0.1"
     local tool_dir="$out_dir/$tool_type-$tool_version"
-    local tool_archive="./existing/findbugs-noUpdateChecks-$tool_version.zip"
+
+
+    if [[ -d "$tool_dir" ]]; then
+	echo "Error: Tool directory already exists: $tool_dir";
+	exit ;
+    fi
 
     mkdir -p $tool_dir/noarch/{in-files,swamp-conf}
-    
+
+    copy_file $tool_url "$tool_dir/noarch/in-files"
+    local exit_code=$?
+
+    if [[ $exit_code -ne 0 ]]; then
+	echo "Error: copying $tool_url failed, exit code returne: $exit_code"
+	exit 1;
+    fi
+
+    local tool_archive="$tool_dir/noarch/in-files/$(basename $tool_url)"
+
+    declare -a valid_plugins
+    for url in ${plugins[*]}; do
+	copy_file $url  "$tool_dir/noarch/in-files"
+	exit_code=$?
+	if [[ $exit_code -ne 0 ]]; then
+	    echo "Warning: copying $tool_url failed, exit code returne: $exit_code"
+	else
+	    valid_plugins[${#valid_plugins[*]}]="$(basename $url)"
+	fi
+    done
+
     local tool_invoke_file="$tool_dir/noarch/in-files/tool-invoke.txt"
     create_invoke_file $tool_invoke_file
 
     local tool_defaults_conf="$tool_dir/noarch/in-files/tool-defaults.conf"
-    create_tool_defaults_conf $tool_defaults_conf
+    create_tool_defaults_conf $tool_defaults_conf $(printf "\${VMINPUTDIR}/%s:" "${valid_plugins[@]}")
 
     local tool_conf="$tool_dir/noarch/in-files/tool.conf"
 
@@ -86,6 +131,69 @@ tool-use-input-file=yes
 EOF
 
     md5 $tool_dir
+}
+
+readonly USAGE_STR="
+Usage:
+  $0 [(-O|--outdir) <path-to-output-dir>]? [(-R|--url) <url-for-the-archive]? [(-P|--plugin) <url-for-plugin>]* <version>
+
+Optional arguments:
+  [(-O|--outdir) <path-to-output-dir>]  #Path to the directory to create/copy files. Default is \$PWD
+  [(-R|--url) <url-for-the-archive] URL for the tool, If the url starts with http(s), file is downloaded from the internet
+  [(-P|--plugin) <url-for-plugin>] URL for a plugin, multiple plugins can be specified  
+Required arguments:
+  <version> Version number of the tool
+"
+
+function main {
+
+    local version=
+    local out_dir=
+    local tool_url=
+    declare -a plugins
+
+    if [[ $# -lt 1 ]]; then
+	echo -e "$USAGE_STR"
+	exit 1;
+    fi
+
+    while test $# -gt 0; do
+	local key="$1"
+
+	case "$key" in
+	    (-R|--url)
+            tool_url="$2"; 
+	    shift; 
+	    ;;
+	    (-o|-O|--outdir)
+            out_dir="$2"; 
+	    shift;
+	    ;;
+	    (-P|--plugin)
+	    plugins[${#plugins[*]}]="$2"
+	    shift;
+	    ;;
+	    (-h|-H|--help)
+            echo -e "$USAGE_STR";
+	    exit 0;
+	    ;;
+	    ([[:digit:]][.][[:digit:]][.][[:digit:]])
+	    version="$key";
+	    ;;
+	esac
+	shift;
+    done
+
+    if [[ -z $version ]]; then
+	echo "Error: Need a version number as argument"
+	echo -e "$USAGE_STR"
+	exit 1
+    fi
+
+    tool_url="${tool_url:-http://prdownloads.sourceforge.net/findbugs/findbugs-noUpdateChecks-$version.zip}"
+    out_dir="${out_dir:-$PWD}"
+
+    create_all "$version" "$out_dir" "$tool_url" "${plugins[@]}"
 }
 
 main $@
